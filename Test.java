9242faa6d733ed29a3f9b6d94435d6b9f9dfdd58
arunptm1 <arunkumarptm2003@gmail.com>
2026-04-16 11:17:44 +0530

    Change output message from 'sum' to 'total'

--- Test.java
+++ Test.java
@@ -5,7 +5,7 @@ public class Test {
     int a=10;
     int b=20;
     int result = a+b;
-    System.out.println("The sum is " +result);
+    System.out.println("The total is " +result);
     
   }
   static void sub(){
